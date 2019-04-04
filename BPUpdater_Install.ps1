f61e4158a9d157ee5dbf6edbc388198cf1f0644e
Matthew Russell <matthew.russell@adit.net.au>
2019-04-04 17:35:31 +1000

    Update BPUpdater_Install.ps1

--- BPUpdater_Install.ps1
+++ BPUpdater_Install.ps1
@@ -11,4 +11,21 @@ https://github.com/ScriptedAdventures
 https://www.scriptedadventures.net/
 #>
 
-$ScheduledAction = New-ScheduledTaskAction -Execute 'C:\ProgramData\BPUpdater'
+#set up dir
+$ProgDataDir = $env:ALLUSERSPROFILE + "\BPUpdater"
+$PBSDataDir = $ProgDataDir + "\PBSData"
+$LogDir = $ProgDataDir + "\Log"
+$ScriptLocation = $ProgDataDir + "\BP_PBS_Updater.ps1"
+
+#other information
+$PBSShareName = "PBSData$"
+
+#scheduled task paramaters 
+$ScheduledAction = New-ScheduledTaskAction -Execute $($ScriptLocation)
+$ScheduledTrigger = New-ScheduledTaskTrigger -Daily -At 2AM
+
+
+#create share out of $PBSDataDir (admin share)
+New-SmbShare -Name $PBSShareName -Path $PBSDataDir -Description "Admin Share for PBS Data Update Files" 
+
+
