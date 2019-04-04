5d2929ee4167661e9ba5393359480c5e1ae99af3
Matt <therealmankid@gmail.com>
2019-04-04 23:16:00 +1000

    Update BPUpdater_Install.ps1

--- BPUpdater_Install.ps1
+++ BPUpdater_Install.ps1
@@ -11,21 +11,44 @@ https://github.com/ScriptedAdventures
 https://www.scriptedadventures.net/
 #>
 
-#set up dir
+Import-Module ActiveDirectory
+
+#Password Generator
+function Make-Password {
+    $PasswordSamplesArray = "!@#$%^&*()1234567890qwertyuiopasdfghjklzxcvbnm;,./{}:<>?".tochararray()
+    $PasswordOut = (($PasswordSamplesArray | Get-Random -Count '24') -join '')
+    return $PasswordOut
+    }
+$ADPassword = Make-Password
+
+#AD User Creation for Scheduled Task
+$ADUserName = "BPUpdater.svc"
+$ADDescription = "Service Account For BPUpdater Scheduled Task"
+$ADDisplayName = "BP Updater Service Account"
+$ADUser = $env:USERDNSDOMAIN + $ADUserName
+
+New-ADUser -Name $ADUserName -AccountPassword $ADPassword -Description $ADDescription -DisplayName $ADDisplayName -PasswordNeverExpires
+Add-ADGroupMember -Identity "Domain Admins" -Members $ADUserName
+
+#set up dir, and share name
 $ProgDataDir = $env:ALLUSERSPROFILE + "\BPUpdater"
 $PBSDataDir = $ProgDataDir + "\PBSData"
 $LogDir = $ProgDataDir + "\Log"
 $ScriptLocation = $ProgDataDir + "\BP_PBS_Updater.ps1"
-
-#other information
 $PBSShareName = "PBSData$"
+$MachineName = hostname
+$ShareUNC = "\\" + $MachineName + "\" + $PBSShareName
 
 #scheduled task paramaters 
-$ScheduledAction = New-ScheduledTaskAction -Execute $($ScriptLocation)
-$ScheduledTrigger = New-ScheduledTaskTrigger -Daily -At 2AM
+$Action = New-ScheduledTaskAction -Execute $($ScriptLocation)
+$Trigger = New-ScheduledTaskTrigger -Daily -At 2AM
+$Principal = $ADUser
+Register-ScheduledTask -TaskName "BPUpdater Daily" -Trigger $Trigger -User $Principal -Action $Action
 
 
-#create share out of $PBSDataDir (admin share)
-New-SmbShare -Name $PBSShareName -Path $PBSDataDir -Description "Admin Share for PBS Data Update Files" 
+#checks, if not found will create share out of $PBSDataDir (admin share)
+if(!(Test-Path $ShareUNC)){
+    New-SmbShare -Name $PBSShareName -Path $PBSDataDir -Description "Admin Share for PBS Data Update Files" 
+}
 
 
